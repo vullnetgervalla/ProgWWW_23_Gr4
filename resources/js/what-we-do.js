d636894ca86ec404b5e54f31906eaf14b176de50
Erjon Asllani <erjonasllani999@gmail.com>
2024-01-12 03:26:17 +0100

    Added website logo

--- resources/js/what-we-do.js
+++ resources/js/what-we-do.js
@@ -1,10 +1,9 @@
 window.onload = () => {
-  const maxProjectTitleHeight = Math.max(...Array.from(document.querySelectorAll('.project-title')).map((title) => title.clientHeight));
-  
+	const maxProjectTitleHeight = Math.max(...Array.from(document.querySelectorAll('.project-title')).map((title) => title.clientHeight));
+	
 	document.querySelectorAll('.project-title').forEach((title) => {
 		if (title.clientHeight < maxProjectTitleHeight) {
 			title.style.marginBottom = `${maxProjectTitleHeight - title.clientHeight}px`;
-			console.log('margin added');
 		}
 	});
 };
